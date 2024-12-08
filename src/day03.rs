c9db512386c8a0cff4c46624137c087f7882511b
Jason Duffy <jasonsduffy@gmail.com>
2024-12-08 17:19:45 -0500

    day03

--- src/day03.rs
+++ src/day03.rs
@@ -1,74 +1,159 @@
-use std::str::Chars;
-
 use crate::input;
+use regex::Regex;
 
 pub fn run() {
     let lines = input::get_lines("day03");
 
-    println!("Part1: {:?}", sum_of_mul(&lines));
+    println!("Part1: {:?}", sum_of_mul(&lines.join("")));
+    println!("Part2: {:?}", sum_of_enabled_mul(&lines.join("")));
 }
 
-fn sum_of_mul(lines: &[String]) -> i32 {
-    let input = lines.join("");
-    let mut chars = input.chars();
-    let mut total = 0;
+#[derive(Debug, PartialEq)]
+enum Token {
+    Mul,
+    LeftParen,
+    RightParen,
+    Comma,
+    Do,
+    DoNot,
+    Number(i32),
+    EOF,
+}
 
-    let mut searching = true;
-    while searching {
-        if find_mul_keyword(&mut chars) {
-            if let (Some(x), Some(',')) = get_number(&mut chars) {
-                if let (Some(y), Some(')')) = get_number(&mut chars) {
-                    total += x * y;
+struct Scanner {
+    source: String,
+    start: usize,
+    current: usize,
+}
+
+// Leaving this around. Let's see if they make us do more complicated work than regex can handle.
+// It doesn't work quite right yet, but can be a good starting point.
+impl Scanner {
+    fn new(source: String) -> Self {
+        Self {
+            source,
+            start: 0,
+            current: 0,
+        }
+    }
+
+    fn scan_tokens(&mut self) -> Vec<Token> {
+        let mut tokens = vec![];
+
+        while !self.is_at_end() {
+            self.start = self.current;
+            match self.scan_token() {
+                Some(token) => tokens.push(token),
+                None => (),
+            }
+        }
+
+        tokens.push(Token::EOF);
+
+        println!("{:?}", tokens);
+
+        tokens
+    }
+
+    fn scan_token(&mut self) -> Option<Token> {
+        let c = self.advance();
+
+        match c {
+            Some('(') => Some(Token::LeftParen),
+            Some(')') => Some(Token::RightParen),
+            Some(',') => Some(Token::Comma),
+            Some('m') => {
+                if self.next_is('u') && self.next_is('l') {
+                    Some(Token::Mul)
+                } else {
+                    None
                 }
             }
-        } else {
-            searching = false;
-        }
-    }
-    total
-}
-
-fn find_mul_keyword(chars: &mut Chars) -> bool {
-    loop {
-        let mut n = chars.next();
-        while n.is_some() {
-            let c = n.unwrap();
-            if c == 'm' {
-                break;
+            Some('d') => {
+                if self.next_is('o') {
+                    if self.next_is('n') && self.next_is('\'') && self.next_is('t') {
+                        Some(Token::DoNot)
+                    } else {
+                        Some(Token::Do)
+                    }
+                } else {
+                    None
+                }
             }
-            n = chars.next();
-        }
-        if n.is_none() {
-            return false;
-        }
-        if find_char(chars, 'u') && find_char(chars, 'l') && find_char(chars, '(') {
-            return true;
+            Some(c) => {
+                if c.is_digit(10) {
+                    self.number()
+                } else {
+                    None
+                }
+            }
+            None => None,
         }
     }
-}
 
-fn get_number(chars: &mut Chars) -> (Option<i32>, Option<char>) {
-    let mut found = String::new();
+    fn number(&mut self) -> Option<Token> {
+        while self.peek().is_digit(10) {
+            self.advance();
+        }
 
-    let mut peekable = chars.peekable();
+        let value = self.source[self.start..self.current]
+            .parse::<i32>()
+            .unwrap();
 
-    let next;
+        Some(Token::Number(value))
+    }
 
-    loop {
-        let p = peekable.peek().cloned();
-        if p.unwrap_or_default().is_digit(10) {
-            found.push(peekable.next().unwrap());
+    fn next_is(&mut self, expected: char) -> bool {
+        if self.is_at_end() || self.source.chars().nth(self.current) != Some(expected) {
+            false
         } else {
-            next = peekable.peek().cloned();
-            break;
+            self.current += 1;
+
+            true
         }
     }
-    (found.parse::<i32>().ok(), next)
+
+    fn peek(&self) -> char {
+        if self.is_at_end() {
+            '\0'
+        } else {
+            self.source.chars().nth(self.current).unwrap_or('\0')
+        }
+    }
+
+    fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
+    }
+
+    fn advance(&mut self) -> Option<char> {
+        let result = self.source.chars().nth(self.current);
+        self.current += 1;
+
+        result
+    }
 }
 
-fn find_char(chars: &mut Chars, c: char) -> bool {
-    let result = chars.next();
-    result == Some(c)
+pub fn sum_of_mul(input: &str) -> i32 {
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let mut sum = 0;
+    for (_, [x, y]) in re.captures_iter(input).map(|c| c.extract()) {
+        sum += x.parse::<i32>().unwrap() * y.parse::<i32>().unwrap();
+    }
+    sum
+}
+
+pub fn sum_of_enabled_mul(input: &str) -> i32 {
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|(d)(o)(?:n't)?\(\)").unwrap();
+    let mut sum = 0;
+    let mut enabled = true;
+    for (s, [x, y]) in re.captures_iter(input).map(|c| c.extract()) {
+        match s {
+            "do()" => enabled = true,
+            "don't()" => enabled = false,
+            _ => sum += x.parse::<i32>().unwrap() * y.parse::<i32>().unwrap() * enabled as i32,
+        }
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -81,7 +166,7 @@ mod tests {
             "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))".to_string(),
         ];
 
-        assert_eq!(sum_of_mul(&example), 161);
+        assert_eq!(sum_of_mul(&example.join("")), 161);
     }
 
     #[test]
@@ -91,6 +176,15 @@ mod tests {
             "*,/~):^[mul(802,939~;!:@mul(656,831)}]?~mul(92,389)/".to_string(),
         ];
 
-        assert_eq!(sum_of_mul(&example), 1_630_869);
+        assert_eq!(sum_of_mul(&example.join("")), 1_630_869);
+    }
+
+    #[test]
+    fn part2_example() {
+        let example = vec![
+            "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))".to_string(),
+        ];
+
+        assert_eq!(sum_of_enabled_mul(&example.join("")), 48);
     }
 }
